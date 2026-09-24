ce6d97cf5cd342748ff6f1850c3b49c70509158c
BLACK HAWK <38820780+black-hawk95@users.noreply.github.com>
2026-09-24 21:25:54 +0200

    Prepare Extra Special Slots v1.1.0: server/ModMetadata.cs

--- server/ModMetadata.cs
+++ server/ModMetadata.cs
@@ -9,7 +9,7 @@ public sealed record ModMetadata : IModMetadata
     public string ModGuid { get; init; } = "com.blackhawk.extraspecialslots";
     public string Name { get; init; } = "ExtraSpecialSlots";
     public string Author { get; init; } = "BLACK HAWK";
-    public SemanticVersioning.Version Version { get; init; } = new("1.0.1");
+    public SemanticVersioning.Version Version { get; init; } = new("1.1.0");
     public SemanticVersioning.Range SptVersion { get; init; } = new(">=4.1.6 <4.1.7");
     public string License { get; init; } = "MIT";
     public string? Url { get; init; } = null;
